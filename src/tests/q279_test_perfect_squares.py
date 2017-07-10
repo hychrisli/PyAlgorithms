ae2ccf6c181b17d184e2fa4d47e3214b66e634c6
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-07-09 17:50:38 -0700

    q279 perfect squares

--- src/tests/q279_test_perfect_squares.py
+++ src/tests/q279_test_perfect_squares.py
@@ -1,4 +1,8 @@
 from src.base.test_cases import TestCases
 
 class PerfectSquaresTestCases(TestCases):
-    pass
+
+    def __init__(self):
+        super(PerfectSquaresTestCases, self).__init__()
+        self.__add_test_case__("Example 1", 12, 3)
+        self.__add_test_case__("Example 2", 13, 2)
