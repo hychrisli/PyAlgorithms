287634ead5af2da4c71656b29fc4a94c2e96b2f9
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-07-11 22:24:09 -0700

    q 64 min path sum

--- src/tests/q064_test_min_path_sum.py
+++ src/tests/q064_test_min_path_sum.py
@@ -1,6 +1,7 @@
 from src.base.test_cases import TestCases
 
-class MinPathSum(TestCases):
+class MinPathSumTestCases(TestCases):
 
     def __init__(self):
-        super(MinPathSum, self).__init__()
+        super(MinPathSumTestCases, self).__init__()
+        self.__add_test_case__("Test 1", [[1,0,2,1],[1,2,0,1], [2,0,1,1],[1,2,0,1],[1,1,0,2],[1,1,0,1]], 5)
