ab4559cff699703655c1fe73aba3791edf6b061d
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-07-18 17:50:29 -0700

    q449 serialize binary search tree

--- src/solutions/part2/q449_de_serial_bst.py
+++ src/solutions/part2/q449_de_serial_bst.py
@@ -1,20 +1,22 @@
 from src.base.solution import Solution
 from src.tests.part2.q449_test_de_serial_bst import DeSerialBstTestCases
+from src.structures.treenode import TreeNode
 
 class DeSerialBst(Solution):
     def run_test(self, input):
-        code = Codec()
-        retur
+        serial_bst = Codec()
+        res1 = serial_bst.serialize(input[0])
+        res2 = serial_bst.deserialize(input[1])
+        return (res1, res2)
 
     def gen_test_cases(self):
         return DeSerialBstTestCases()
 
     def verify_output(self, test_output, output):
-        return super(DeSerialBst, self).verify_output(test_output, output)
+        return test_output[0] == output[0] and test_output[1] == output[1]
 
     def print_output(self, output):
-        super(DeSerialBst, self).print_output(output)
-
+        print((output[0], output[1].get_tree_str()))
 
 class Codec:
     def serialize(self, root):
@@ -23,6 +25,18 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        self.vals = []
+        self.preorder_serial(root)
+        return ' '.join(self.vals)
+
+    def preorder_serial(self, root):
+        if not root:
+            self.vals.append('n')
+            return
+        # print(self.vals)
+        self.vals.append(str(root.val))
+        self.preorder_serial(root.left)
+        self.preorder_serial(root.right)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -30,6 +44,8 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        self.chs = data.split(' ')
+        self.n = len(self.chs)
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
--- src/tests/part2/q449_test_de_serial_bst.py
+++ src/tests/part2/q449_test_de_serial_bst.py
@@ -1,8 +1,14 @@
 from src.base.test_cases import TestCases
+from src.mappers.list2treenode import to_complete_binary_tree
+
 
 class DeSerialBstTestCases(TestCases):
 
     def __init__(self):
-        super(DeSerialBst, self).__init__()
-        self.__add_test_case__('Test 1', )
+        super(DeSerialBstTestCases, self).__init__()
+        tree = to_complete_binary_tree([5, 3, 8,1,4,None,9,None,2])
+        data = '5 3 1 n 2 n n 4 n n 8 n 9 n n'
+
+        self.__add_test_case__( 'test 1', (tree, data), (data, tree))
+
 
